f26a0b40ac67d7b01c18320bb17faad7621cce5b
Soumasish Dasgupta <72874054+Soumasish2005@users.noreply.github.com>
2025-03-19 23:05:47 +0530

    Update Kruskal_Algo.c

--- Algorithms/Graph_Algorithms/Kruskal_Algo.c
+++ Algorithms/Graph_Algorithms/Kruskal_Algo.c
@@ -1,97 +1,73 @@
-// C code to implement Kruskal's algorithm
-
 #include <stdio.h>
 #include <stdlib.h>
 
-// Comparator function to use in sorting
-int comparator(const int p1[], const int p2[])
-{
-    return p1[2] - p2[2];
+#define MAX_EDGES 100
+
+// Structure to represent an edge
+typedef struct {
+    int u, v, weight;
+} Edge;
+
+// Comparator function for sorting edges
+int compare(const void *a, const void *b) {
+    return ((Edge *)a)->weight - ((Edge *)b)->weight;
 }
 
-// Initialization of parent[] and rank[] arrays
-void makeSet(int parent[], int rank[], int n)
-{
-    for (int i = 0; i < n; i++) {
-        parent[i] = i;
-        rank[i] = 0;
+// Find the representative (root) of a set
+int find(int parent[], int i) {
+    if (parent[i] != i)
+        parent[i] = find(parent, parent[i]); // Path compression
+    return parent[i];
+}
+
+// Union of two sets
+void unionSets(int parent[], int rank[], int u, int v) {
+    int rootU = find(parent, u);
+    int rootV = find(parent, v);
+
+    if (rootU != rootV) {
+        if (rank[rootU] < rank[rootV])
+            parent[rootU] = rootV;
+        else if (rank[rootU] > rank[rootV])
+            parent[rootV] = rootU;
+        else {
+            parent[rootV] = rootU;
+            rank[rootU]++;
+        }
     }
 }
 
-// Function to find the parent of a node
-int findParent(int parent[], int component)
-{
-    if (parent[component] == component)
-        return component;
+// Function to find MST using Kruskal's Algorithm
+void kruskalMST(Edge edges[], int V, int E) {
+    qsort(edges, E, sizeof(Edge), compare); // Sort edges by weight
 
-    return parent[component]
-           = findParent(parent, parent[component]);
-}
+    int parent[V], rank[V];
+    for (int i = 0; i < V; i++)
+        parent[i] = i, rank[i] = 0;
 
-// Function to unite two sets
-void unionSet(int u, int v, int parent[], int rank[], int n)
-{
-    // Finding the parents
-    u = findParent(parent, u);
-    v = findParent(parent, v);
+    int minCost = 0, edgeCount = 0;
 
-    if (rank[u] < rank[v]) {
-        parent[u] = v;
-    }
-    else if (rank[u] > rank[v]) {
-        parent[v] = u;
-    }
-    else {
-        parent[v] = u;
+    printf("Edge \tWeight\n");
+    for (int i = 0; i < E && edgeCount < V - 1; i++) {
+        int u = edges[i].u, v = edges[i].v, weight = edges[i].weight;
 
-        // Since the rank increases if
-        // the ranks of two sets are same
-        rank[u]++;
-    }
-}
-
-// Function to find the MST
-int kruskalAlgo(int n, int edge[n][3])
-{
-    // First we sort the edge array in ascending order
-    // so that we can access minimum distances/cost
-    qsort(edge, n, sizeof(edge[0]), comparator);
-
-    int parent[n];
-    int rank[n];
-
-    // Function to initialize parent[] and rank[]
-    makeSet(parent, rank, n);
-
-    // To store the minimun cost
-    int minCost = 0;
-    for (int i = 0; i < n; i++) {
-        int v1 = findParent(parent, edge[i][0]);
-        int v2 = findParent(parent, edge[i][1]);
-        int wt = edge[i][2];
-
-        // If the parents are different that
-        // means they are in different sets so
-        // union them
-        if (v1 != v2) {
-            unionSet(v1, v2, parent, rank, n);
-            minCost += wt;
+        if (find(parent, u) != find(parent, v)) {
+            unionSets(parent, rank, u, v);
+            printf("%d - %d \t%d\n", u, v, weight);
+            minCost += weight;
+            edgeCount++;
         }
     }
 
-    return minCost;
+    printf("Minimum Cost: %d\n", minCost);
 }
 
-// Driver code
-int main()
-{
-    int edge[5][3] = { { 0, 1, 10 },
-                       { 0, 2, 6 },
-                       { 0, 3, 5 },
-                       { 1, 3, 15 },
-                       { 2, 3, 4 } };
+// Driver function
+int main() {
+    Edge edges[] = { {0, 1, 10}, {0, 2, 6}, {0, 3, 5}, {1, 3, 15}, {2, 3, 4} };
+    int V = 4, E = 5; // Number of vertices and edges
 
-    printf("%d",kruskalAlgo(5, edge));
+    kruskalMST(edges, V, E);
 
     return 0;
 }
